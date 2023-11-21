982abdab6a2f21374146956b23f7e0521c056164
Johan Flensmark <jokf@itu.dk>
2023-11-21 16:53:28 +0100

    fix(tests): add missing tests from last commit

---
+++ raven/src/test/java/dk/itu/raven/join/SquareTest.java
@@ -0,0 +1,25 @@
+package dk.itu.raven.join;
+
+//junit test for the square class
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.github.davidmoten.rtree2.geometry.Geometries;
+import com.github.davidmoten.rtree2.geometry.Rectangle;
+
+public class SquareTest {
+
+	@Test
+	public void testContains() {
+		Square square = new Square(0, 0, 10);
+		Rectangle inside = Geometries.rectangle(1, 1, 5, 5);
+		Rectangle outside = Geometries.rectangle(11, 11, 15, 15);
+		Rectangle intersect = Geometries.rectangle(7, 7, 15, 15);
+
+		assertTrue(square.contains(inside));
+		assertFalse(square.contains(outside));
+		assertFalse(square.contains(intersect));
+	}
+
+}
