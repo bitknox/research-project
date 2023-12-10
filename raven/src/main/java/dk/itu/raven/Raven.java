a313f58b8af71e05d7fc374a740c35121a1d798d
Johan Flensmark <jokf@itu.dk>
2023-12-10 13:01:36 +0100

    fix(benchmark): extractCells bench

--- raven/src/main/java/dk/itu/raven/Raven.java
+++ raven/src/main/java/dk/itu/raven/Raven.java
@@ -4,9 +4,12 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
 import java.util.Stack;
+import java.util.stream.Collectors;
 
 import org.apache.hadoop.fs.Path;
 import org.locationtech.jts.geom.Coordinate;
@@ -18,43 +21,58 @@ import dk.itu.raven.geometry.Polygon;
 
 import dk.itu.raven.join.RavenJoin;
 import dk.itu.raven.join.Square;
+import dk.itu.raven.util.Pair;
 
 public class Raven {
 
     public static void main(String[] args) throws IOException {
         ArrayList<Long> times = new ArrayList<>();
-        int[] numPoints = { 2000, 4000, 8000, 16000, 32000, 64000, 128000, 256000 };
+        int[] numPoints = { 2000, 2000, 4000, 8000, 16000, 32000, 64000, 128000, 256000 };
         for (int numP : numPoints) {
 
             for (int j = 0; j < 1000; j++) {
-                List<Point> points = new ArrayList<>();
-                double angle = 0;
-                double length = 2.0;
-                double size = 0;
-                points.add(Geometries.point(0, 0));
-                for (int i = 0; i < numP; i++) {
-                    angle += 2 * Math.PI / numP;
-                    Point last = points.get(points.size() - 1);
-                    Point next = Geometries.point(last.x() + Math.cos(angle) * length,
-                            last.y() + Math.sin(angle) * length);
-                    points.add(next);
-                    size = Math.max(size, next.y());
-                    size = Math.max(size, next.x());
-                }
-                Polygon poly = new Polygon(points);
+                Polygon poly = generatePolygon(numP);
                 RavenJoin join = new RavenJoin(null, null);
-                Square square = new Square(0, 0, (int) size + 10);
+                Square square = new Square(-2000, -2000, 4000);
                 long start = System.nanoTime();
-                join.ExtractCellsPolygonBeast(poly, 0, square, square.getSize());
+                join.ExtractCellsPolygon(poly, 0, square, square.getSize());
                 times.add(System.nanoTime() - start);
 
             }
             double avg = calculateAverage(times);
-            System.out.println("Average time: " + avg);
+            System.out.print(avg + ",");
             times.clear();
 
         }
 
+    }
+
+    private static Polygon generatePolygon(int numPoints) {
+        Random r = new Random();
+        final double innerDist = 700;
+        final double outerDist = 2000;
+        double oldDist = (innerDist + outerDist) / 2;
+        double maxJump = 30.0;
+
+        List<Pair<Double, Point>> points = new ArrayList<>();
+        for (int i = 0; i < numPoints; i++) {
+            double angle = 2 * Math.PI * i / numPoints;
+            double dist = Math.max(Math.min((oldDist + maxJump * (r.nextDouble() - 0.5)), outerDist), innerDist);
+            Point p = Geometries.point(Math.cos(angle) * dist, Math.sin(angle) * dist);
+            points.add(new Pair<Double, Point>(angle, p));
+            oldDist = dist;
+        }
+
+        Collections.sort(points, new Comparator<Pair<Double, Point>>() {
+            @Override
+            public int compare(Pair<Double, Point> o1, Pair<Double, Point> o2) {
+                return (int) Math.signum(o1.first - o2.first);
+            }
+        });
+
+        Polygon poly = new Polygon(points.stream()
+                .map(pr -> Geometries.point(pr.second.x() + 2000, pr.second.y() + 2000)).collect(Collectors.toList()));
+        return poly;
     }
 
     private static double calculateAverage(List<Long> marks) {
